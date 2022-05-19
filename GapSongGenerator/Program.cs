a4219452ba65c686c997500ffe60882fca57617d
MJTSgamer <matttersteege@gmail.com>
2022-05-19 08:55:40 +0200

    new update

--- GapSongGenerator/Program.cs
+++ GapSongGenerator/Program.cs
@@ -1,21 +1,45 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace GapSongGenerator
 {
     class Program
     {
-        static void Main()
+        public static class KnownFolder
         {
-            Console.WriteLine("what is the file path?\npaste it down below");
+            public static readonly Guid Downloads = new Guid("374DE290-123F-4565-9164-39C4925E467B");
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags,
+            IntPtr hToken, out string pszPath);
+        
+        static void Main(string[] args)
+        {
+            string path;
+            
+            if (args.Length > 0)
+            {
+                path = args[0];
+            }
+            else
+            {
+                Console.WriteLine("what is the file path?\npaste it down below");
+                path = Console.ReadLine();
+            }
+            
+            string downloadsFolderPath;
+            SHGetKnownFolderPath(KnownFolder.Downloads, 0, IntPtr.Zero, out downloadsFolderPath);
+            
             String line;
 
-            string path = Console.ReadLine();
 
             StreamReader sr = new StreamReader(path);
-            StreamWriter srout = new StreamWriter(path.Replace(".txt", "") + "- output.txt");
+            StreamWriter srout = new StreamWriter(downloadsFolderPath + @"\output.txt");
 
             line = sr.ReadLine();
 
@@ -36,7 +60,18 @@ namespace GapSongGenerator
                     string changedword = words.GetValue(r).ToString();
 
 
-                    string changedwordafter = new String(changedword.Select(r => r == ' ' ? ' ' : '.').ToArray());
+                    char[] charArray = changedword.ToCharArray();
+
+                    string changedwordafter = "";
+
+                    foreach (char c in changedword)
+                    {
+                        string letter = c.ToString();
+                        
+                        letter = Regex.Replace(letter, "[^\"]", "."); //Replace all with "." EXCEPT: " 
+
+                        changedwordafter = new string(charArray);
+                    }
 
                     words.SetValue(changedwordafter, r);
 
